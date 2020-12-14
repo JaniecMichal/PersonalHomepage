c2b39ace42921d73f31a371ed6fab366c2f424af
Michal Janiec <michal.janiec95@gmail.com>
2020-12-14 23:49:52 +0100

    Added Two Sections to Render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Container from "./Container";
 import MainHeader from "./MainHeader";
+import Section from "./Section";
+import skillset from "./skillset";
+import skillsToLearn from "./skillsToLearn";
+import tools from "./images/tools.svg";
+import rocket from "./images/rocket.svg";
 
 function App() {
   return (
     <Container>
       <MainHeader />
+      <Section
+        sectionTitle={"My skillset includes"}
+        sectionIcon={tools}
+        sectionList={skillset}
+      />
+      <Section
+        sectionTitle={"What I want to learn next"}
+        sectionIcon={rocket}
+        sectionList={skillsToLearn}
+      />
     </Container>
   );
 }
