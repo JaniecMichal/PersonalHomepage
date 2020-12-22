4c4ec20c770e4c981bbc0e8b42e117b672b1a3e6
Michal Janiec <michal.janiec95@gmail.com>
2020-12-22 23:34:08 +0100

    Added new condition of displaying depending to loading and error variables

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,27 +1,39 @@
 import React from "react";
+import ConnectionError from "./ConnectionError";
 import Icon from "./images/GithubBlue.svg";
 import ProjectTile from "./ProjectTile";
 import Spinner from "./Spinner";
 import { HeaderTitle, StyledWrapper, SubTitle, TileContainer } from "./styled";
 
-const Portfolio = ({ loading }) => (
-  <StyledWrapper>
-    <header>
-      <img src={Icon} alt="githubIcon" />
-      <HeaderTitle>Portfolio</HeaderTitle>
-      <SubTitle>My recent projects</SubTitle>
-    </header>
-    {!!loading ?
-      <TileContainer>
-        <ProjectTile />
-        <ProjectTile />
-        <ProjectTile />
-        <ProjectTile />
-      </TileContainer>
-      :
-      <Spinner />
+const Portfolio = ({ loading, error }) => {
+
+  const returnCorrectContent = () => {
+    if (loading === true) {
+      return <Spinner />
+    } else if (error === true) {
+      return <ConnectionError />
+    } else {
+      return (
+        <TileContainer>
+          <ProjectTile />
+          <ProjectTile />
+          <ProjectTile />
+          <ProjectTile />
+        </TileContainer>
+      )
     }
-  </StyledWrapper>
-);
+  };
+
+  return (
+    <StyledWrapper>
+      <header>
+        <img src={Icon} alt="githubIcon" />
+        <HeaderTitle>Portfolio</HeaderTitle>
+        <SubTitle>My recent projects</SubTitle>
+      </header>
+      {returnCorrectContent()}
+    </StyledWrapper>
+  )
+};
 
 export default Portfolio;
