44359e065be0ebcf762489e72c48165f2d0ac0e3
Michal Janiec <michal.janiec95@gmail.com>
2021-03-25 23:51:05 +0100

    Adapted styles to new theme pattern

--- src/MainHeader/styled.js
+++ src/MainHeader/styled.js
@@ -2,92 +2,79 @@ import styled, { css } from "styled-components";
 import me from "./images/Me.jpg";
 
 export const StyledHeader = styled.header`
-  max-width:100%;
-  display:grid;
+  max-width: 100%;
+  display: grid;
   grid-template-columns: minmax(100px, 384px) 2fr;
-  grid-template-rows:minmax(100px, 384px);
-  grid-gap:66px;
-  align-items:center;
+  grid-template-rows: minmax(100px, 384px);
+  grid-gap: 66px;
+  align-items: center;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     grid-template-columns: auto;
-    grid-template-rows: repeat(2,minmax(100px,auto));
-    align-items:start;
-    grid-gap:50px;
+    grid-template-rows: repeat(2, minmax(100px, auto));
+    align-items: start;
+    grid-gap: 50px;
   }
 
   @media (max-width: ${({ theme }) => theme.media.extraMobile}) {
     grid-template-columns: auto;
-    grid-template-rows: repeat(2,auto);
+    grid-template-rows: repeat(2, auto);
   }
- 
 `;
 
 export const MyPhoto = styled.div`
-  padding-top:100%;
-  border-radius:50%;
-  background-image:url(${me});
-  background-position:center;
-  background-repeat:no-repeat;
-  background-size:cover;
+  padding-top: 100%;
+  border-radius: 50%;
+  background-image: url(${me});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const About = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:space-evenly;
-  align-items:flex-start;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
 `;
 
 export const Caption = styled.span`
-  font-size:${({ theme }) => theme.fontSize.small};
-  font-weight:${({ theme }) => theme.fontWeight.bold};
-  color:${({ theme }) => theme.color.slateGray};
-  margin-bottom:12px;
-  text-transform:uppercase;
-
-  ${({ darkMode }) => darkMode && css`
-    color:${({ theme }) => theme.color.white};
-  `};
+  font-size: ${({ theme }) => theme.fontSize.small};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  margin-bottom: 12px;
+  text-transform: uppercase;
 `;
 
 export const HeaderTitle = styled.h1`
-  font-weight:${({ theme }) => theme.fontWeight.black};
-  font-size:${({ theme }) => theme.fontSize.theLargest};
-  color:${({ theme }) => theme.color.mineShaft};
-  margin-top:0px;
-  margin-bottom:35px;
+  font-weight: ${({ theme }) => theme.fontWeight.black};
+  font-size: ${({ theme }) => theme.fontSize.theLargest};
+  color: ${({ theme }) => theme.colors.textPrimary};
+  margin-top: 0px;
+  margin-bottom: 35px;
 
-  ${({ darkMode }) => darkMode && css`
-    color:${({ theme }) => theme.color.white};
-  `};
+  ${({ darkMode }) =>
+    darkMode &&
+    css`
+      color: ${({ theme }) => theme.colors.body.text};
+    `};
 
   @media (max-width: ${({ theme }) => theme.media.extraMobile}) {
-    font-size:${({ theme }) => theme.fontSize.large};
+    font-size: ${({ theme }) => theme.fontSize.large};
   }
 `;
 
 export const MyDescription = styled.p`
-  font-weight:${({ theme }) => theme.fontWeight.normal};
-  font-size:${({ theme }) => theme.fontSize.large};
-  color:${({ theme }) => theme.color.slateGray};
-  margin-top:0px;
-  margin-bottom:32px;
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  font-size: ${({ theme }) => theme.fontSize.large};
+  margin-top: 0px;
+  margin-bottom: 32px;
   line-height: 1.4;
 
-  ${({ darkMode }) => darkMode && css`
-    color:${({ theme }) => theme.color.white};
-  `};
-
   @media (max-width: ${({ theme }) => theme.media.extraMobile}) {
-    font-size:${({ theme }) => theme.fontSize.normal};
+    font-size: ${({ theme }) => theme.fontSize.normal};
   }
 `;
 
 export const ContactLink = styled.a`
-text-decoration: none;
-
-  &:visited{
-  color: ${({ theme }) => theme.color.white};
-}
+  text-decoration: none;
 `;
